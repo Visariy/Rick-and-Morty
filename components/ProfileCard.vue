8518dca688ae1de39721cc7efccdc8b4c981c8f7
Visariy <nikongb2@yandex.ru>
2023-08-04 09:44:48 +0300

    character page logic finish

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        1
+    <div class="profile-card-container">
+        <div class="name">
+            {{ character.name }}
+        </div>
+        <div class="species">
+            {{ character.species }}
+        </div>
+        <div class="location">
+            {{ character.location.name }}
+        </div>
+        <div class="image-container">
+            <img class="image" :src="character.image" alt="img" />
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { ICharacter } from '~/interfaces/ICharacter';
+
+
+defineProps<({character: ICharacter})>()
+
+</script>
