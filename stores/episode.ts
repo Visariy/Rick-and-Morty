898eaee12047bef9031c67d30c96cde40e543d08
Visariy <nikongb2@yandex.ru>
2023-08-05 12:30:30 +0300

    delete unusable variable

--- stores/episode.ts
+++ stores/episode.ts
@@ -4,14 +4,11 @@ import { IEpisode } from "~/interfaces/IEpisode";
 
 export const useEpisodeStore = defineStore("episodeStore", () => {
   const episodeInfo = ref<IEpisode>();
-  const isLoading = ref(false);
 
   const getEpisodeByUrl = async (url: string) => {
     try {
-      isLoading.value = true;
       const response = await ApiService.getEpisodeByUrl(url);
       episodeInfo.value = response.data;
-      isLoading.value = false;
     } catch (e) {
       console.log(e);
     }
@@ -19,13 +16,11 @@ export const useEpisodeStore = defineStore("episodeStore", () => {
 
   const getEpisodeById = async (id: string) => {
     try {
-      isLoading.value = true;
       return await ApiService.getEpisodeById(id);
-      isLoading.value = false;
     } catch (e) {
       console.log(e);
     }
   };
 
-  return { getEpisodeByUrl, getEpisodeById, episodeInfo, isLoading };
+  return { getEpisodeByUrl, getEpisodeById, episodeInfo };
 });
